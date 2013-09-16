9b997cf89b54f191f6318edd74d710cbb53c2056
Chris McFarlen <mcfarlen@yahoo-inc.com>
2013-09-15 21:56:49 -0500

    keyboard support

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.rxtx/rxtx "2.2pre2"]
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [seesaw "1.4.3"]]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :main visca.async)
--- src/visca/async.clj
+++ src/visca/async.clj
@@ -1,6 +1,7 @@
 (ns visca.async
   (:require [visca.core :as core]
-            [clojure.core.async :as async :refer :all]))
+            [clojure.core.async :as async :refer :all]
+            [seesaw.core :as seesaw]))
 
 
 (defn exchange [port ch]
@@ -13,29 +14,61 @@
           (println "resp" (core/dump-message resp))
           (>! ch resp)))))
 
+(def curr-key (atom 0))
+
+(defn handle-key-pressed [p e]
+  (let [c (.getKeyCode e)]
+    (if (not= @curr-key c)
+      (do (reset! curr-key c)
+        (case (.getKeyCode e)
+          37 (core/write-command p (core/move-left 10))
+          38 (core/write-command p (core/move-up 10))
+          39 (core/write-command p (core/move-right 10))
+          40 (core/write-command p (core/move-down 10)))))))
+
+(defn handle-key-released [p e]
+  (do
+    (core/write-command p (core/move-stop))
+    (reset! curr-key 0)))
+
+
 (defn -main
   [& args]
-  (let [p (core/open (first args))
-        ch (chan)]
-    (response-pump p ch)
-    (<!! (go (do
-               (core/write-command p core/IF_Address)
-               (<! ch)
+  (let [p (core/open (first args))]
+    (seesaw/invoke-later
+      (seesaw/listen 
+        (-> (seesaw/frame :title "Hello"
+               :content "Hello, Seesaw"
+               :on-close :exit
+                          )
+         seesaw/pack!
+         seesaw/show!)
+        :key-pressed (partial handle-key-pressed p)
+        :key-released (partial handle-key-released p)))))
 
-               (core/write-command p core/IF_Clear)
-               (<! ch)
-
-               (core/write-command p [ 0x81 0x01 0x06 0x02 0x10 0x10 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0xff ])
-               (<! ch)
-               (<! ch)
-
-               (core/write-command p [ 0x81 0x01 0x06 0x02 0x10 0x10 0x00 0x02 0x00 0x00 0x00 0x00 0x00 0x00 0xff ])
-               (<! ch)
-               (<! ch)
-
-               (core/write-command p [ 0x81 0x01 0x06 0x02 0x10 0x10 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0xff ])
-               (<! ch)
-               (<! ch)
-               
-               )))))
-
+; (defn -main
+;   [& args]
+;   (let [p (core/open (first args))
+;         ch (chan)]
+;     (response-pump p ch)
+;     (<!! (go (do
+;                (core/write-command p core/IF_Address)
+;                (<! ch)
+; 
+;                (core/write-command p core/IF_Clear)
+;                (<! ch)
+; 
+;                (core/write-command p [ 0x81 0x01 0x06 0x02 0x10 0x10 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0xff ])
+;                (<! ch)
+;                (<! ch)
+; 
+;                (core/write-command p [ 0x81 0x01 0x06 0x02 0x10 0x10 0x00 0x02 0x00 0x00 0x00 0x00 0x00 0x00 0xff ])
+;                (<! ch)
+;                (<! ch)
+; 
+;                (core/write-command p [ 0x81 0x01 0x06 0x02 0x10 0x10 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0xff ])
+;                (<! ch)
+;                (<! ch)
+;                
+;                )))))
+; 
